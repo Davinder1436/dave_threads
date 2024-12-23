8bb11c201d87b46325f4d8cffe45603068f917bf
Davinder1436 <132595891+Davinder1436@users.noreply.github.com>
2024-12-24 00:34:52 +0530

    Delete build directory
    Unwanted

--- build/graphql/user/index.js
+++
@@ -1,5 +0,0 @@
-import { typeDefs } from "./typedef.js";
-import { resolvers } from "./resolvers.js";
-import { queries } from "./queries.js";
-import { mutations } from "./mutations.js";
-export const User = { typeDefs, resolvers, queries, mutations };
--- build/graphql/user/mutations.js
+++
@@ -1,9 +0,0 @@
-export const mutations = `#graphql
-
-createUser(firstName: String!, lastName: String! ,email: String!,password: String!):String
-
-createPost(content: String!,imageUrl: String, videoUrl: String ):String
-
-likePost(userid:ID!,postId:ID!):String
-
-`;
